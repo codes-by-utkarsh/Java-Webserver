f5f972b594038a9860ee4af1ad50b8ee7209f1ca
codes-by-utkarsh <usrivastava2023@gmail.com>
2025-12-01 12:12:39 +0530

    Inet address resolve

--- SingleThreaded/Client.java
+++ SingleThreaded/Client.java
@@ -1,2 +1,16 @@
-public class Client {
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client
+{
+    public void run() throws IOException
+    {
+        int port = 8010;
+        InetAddress address = InetAddress.getByName("localhost");
+        try
+        {
+            Socket socket = new Socket(address, port);
+        }
+    }
 }
