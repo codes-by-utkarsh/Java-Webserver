a8b13eefbebfc1297764ff26d74b7db53296ad83
codes-by-utkarsh <usrivastava2023@gmail.com>
2025-12-01 12:06:20 +0530

    Added single and multi threaded webserver

---
+++ SingleThreaded/Server.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+public class Server
+{
+    public void run() throws IOException
+    {
+        int port = 8010;
+        ServerSocket socket = new ServerSocket(port);
+        socket.setSoTimeout(10000);
+        System.out.println("Server is running on port " + port);
+        while (true) {
+            try{
+                Socket acceptedSocket = socket.accept();
+                System.out.println("Client connected To InetAddress"+acceptedSocket.getInetAddress()+" RemoteSocketAddress: "+acceptedSocket.getRemoteSocketAddress());
+                PrintWriter toClient = new PrintWriter(acceptedSocket.getOutputStream(), true);
+                toClient.println("Hello From the Server");
+                toClient.close();
+                acceptedSocket.close();
+            } catch (SocketTimeoutException e) {
+                System.out.println("Waiting for clients...");
+            } catch (IOException e) {
+                System.out.println("Error accepting client connection");
+                e.printStackTrace();
+            }   
+            
+            
+        }
+    }
+    
+    
+    public static void main(String[] args) {
+        try{
+            Server server = new Server();
+            server.run();
+        } catch (IOException e) {
+            System.out.println("Error starting server");
+            e.printStackTrace();
+        }
+    }
+}
