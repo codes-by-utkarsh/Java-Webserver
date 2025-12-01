28b2a224d1d766f663d3b2a425aac5ebe4c706fd
codes-by-utkarsh <usrivastava2023@gmail.com>
2025-12-01 12:17:21 +0530

    Printwriter, BufferedReader Implemented

--- SingleThreaded/Client.java
+++ SingleThreaded/Client.java
@@ -1,16 +1,25 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 
 public class Client
 {
-    public void run() throws IOException
+    public void run() throws IOException, UnknownHostException
     {
         int port = 8010;
         InetAddress address = InetAddress.getByName("localhost");
-        try
-        {
-            Socket socket = new Socket(address, port);
-        }
+        Socket socket = new Socket(address, port);
+        PrintWriter toSocket = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        toSocket.println("Hello From the Client");
+        String line=fromSocket.readLine();
+        System.out.println("Response from the socket is "+line);
     }
+
+    public st
 }
