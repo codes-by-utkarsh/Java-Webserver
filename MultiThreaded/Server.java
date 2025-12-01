3eb9487dfb1e5a63485273d223b1534a3d2e12c4
codes-by-utkarsh <usrivastava2023@gmail.com>
2025-12-01 13:39:46 +0530

    Mulithreaded server init

---
+++ MultiThreaded/Server.java
@@ -0,0 +1,12 @@
+import
+
+public class Server {
+    public static void main(String[] args) {
+        int port = 8010;
+        try
+        {
+            ServerSocket serverSocket = new ServerSocket(port);
+            serverSocket.getS
+        }
+    }
+}
