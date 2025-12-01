3a054d1878a5b893c370cfb5a669266fb3ee7215
codes-by-utkarsh <usrivastava2023@gmail.com>
2025-12-01 13:46:16 +0530

    input/output streams connection established

--- MultiThreaded/Server.java
+++ MultiThreaded/Server.java
@@ -1,12 +1,41 @@
-import
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.function.Consumer;
 
 public class Server {
+    
+    public Consumer<Socket> getConsumer() {
+        return clientSocket -> {
+            try {
+                PrintWriter toClient=new PrintWriter(clientSocket.getOutputStream(), true);
+                toClient.println("Hello From the Server");
+                toClient.close();
+            }
+            catch (IOException e) {
+                System.out.println("Error handling client");
+                e.printStackTrace();
+            }
+        };
+    }
+    
     public static void main(String[] args) {
         int port = 8010;
         try
         {
             ServerSocket serverSocket = new ServerSocket(port);
-            serverSocket.getS
+            serverSocket.getSoTimeout(10000);
+            System.out.println("Server is running on port " + port);
+            while (true) {
+                Socket acceptedSocket = serverSocket.accept();
+                System.out.println("Client connected to the server");
+                Thread thread = new Thread();
+                thread.start();
+            }
+        } catch (IOException e) {
+            System.out.println("Error starting server");
+            e.printStackTrace();
         }
     }
 }
